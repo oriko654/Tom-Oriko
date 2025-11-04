18a044a379f91a01327833120a9b8807138b601d
oriko654 <tomastommy654@gmail.com>
2025-11-04 11:20:32 +0300

    Add files via upload

---
+++ Mywebsite.js
@@ -0,0 +1,14 @@
+  function submitForm() {
+      let email = document.getElementById('email').value.trim();
+      let message = document.getElementById('message').value.trim();
+
+      let name = document.getElementById('name').value.trim();
+     if (name === '' || email === '' || message === '') {
+        alert("Please fill in all fields! 🔒");
+      } else {
+        alert("Message sent successfully! 🚀 We'll get back to you.");
+        // Reset form
+        document.getElementById('contactForm').reset();
+      }
+    }
+    
